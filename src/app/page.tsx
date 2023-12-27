658d4040cd450d636017054c72cadb5309c3823a
sim0wn <61605711+sim0wn@users.noreply.github.com>
2023-12-27 16:42:42 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,53 @@
+export default function Page() {
+  return (
+    <main>
+      <section id="short-resume">
+        <h1>Hello, friend.</h1>
+        <p>I'm a developer and cybersecurity enthusiast.</p>
+      </section>
+      <section id="terminal">
+        <div id="title-bar">
+          <h3>My Work</h3>
+        </div>
+        <nav>
+          <menu>
+            <li>Projects</li>
+            <li>Write-Ups</li>
+          </menu>
+        </nav>
+        <div id="content">
+          <div id="projects">
+            <article>
+              <h3>Lorem ipsum</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Magnam rerum error eveniet vitae voluptatum officia quae
+                obcaecati quasi itaque voluptates? Soluta laborum suscipit
+                repellendus at illum ipsa rem tenetur architecto.
+              </p>
+            </article>
+            <article>
+              <h3>Lorem ipsum</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+                obcaecati, minima ipsum deserunt mollitia eius voluptatibus
+                delectus. Porro, officiis! Sequi illum iusto ipsa blanditiis
+                maiores corporis voluptatum dolores nulla ab.
+              </p>
+            </article>
+            <article>
+              <h3>Lorem ipsum</h3>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab
+                veniam odit blanditiis facilis aliquid quasi labore
+                consequuntur, quidem suscipit voluptate totam rerum,
+                accusantium laborum cupiditate pariatur harum magnam eligendi
+                maiores.
+              </p>
+            </article>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
