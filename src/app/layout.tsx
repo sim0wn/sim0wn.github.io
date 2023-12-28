91fdcbb5a915ef7b20bb65eeb185988057688e66
sim0wn <61605711+sim0wn@users.noreply.github.com>
2023-12-28 11:18:06 -0300

    changed background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body
-        className={`${glegoo.className} bg-purple-500 gap-4 w-screen h-screen flex flex-col text-white`}
+        className={`${glegoo.className} bg-purple-600 gap-4 w-screen h-screen flex flex-col text-white`}
       >
         <Header />
         {children}
