db89b7cabb5fedf5da15c91cf57eebb15232981a
sim0wn <61605711+sim0wn@users.noreply.github.com>
2023-12-28 12:20:00 -0300

    added hackthebox and tryhackme social links

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -10,15 +10,20 @@ export default function Footer() {
     <footer className="bg-purple-700 flex flex-row px-4 py-6 h-16 justify-center align-middle gap-3">
       <SocialIcon
         href="http://lattes.cnpq.br/4781391320784524"
-        icon={'academicons:lattes'}
+        icon="academicons:lattes"
       />
       <SocialIcon
-        href="https://github.com/sim0wn/"
-        icon={'fa-brands:github'}
+        href="https://app.hackthebox.com/profile/143157"
+        icon="simple-icons:hackthebox"
+      />
+      <SocialIcon href="https://github.com/sim0wn/" icon="fa-brands:github" />
+      <SocialIcon
+        href="https://tryhackme.com/p/sim0wn"
+        icon="simple-icons:tryhackme"
       />
       <SocialIcon
         href="https://www.linkedin.com/in/halissoncruz/"
-        icon={'fa6-brands:linkedin-in'}
+        icon="fa6-brands:linkedin-in"
       />
     </footer>
   )
