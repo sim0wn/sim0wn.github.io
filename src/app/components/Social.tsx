4097e224a24012d9b41062ff2107383a10efbd2e
sim0wn <61605711+sim0wn@users.noreply.github.com>
2023-12-29 20:56:48 -0300

    customized icon behaviour on hover

--- src/app/components/Social.tsx
+++ src/app/components/Social.tsx
@@ -8,7 +8,7 @@ interface SocialProps extends LinkProps {
 export function Social({ href, icon, ...props }: SocialProps) {
   const styledIcon = React.cloneElement(icon, {
     className:
-      'text-white hover:text-purple-900 hover:bg-white rounded-md p-1 w-full h-14',
+      'transition text-white hover:text-purple-900 hover:scale-105 hover:-translate-y-1 duration-125 ease-out hover:bg-white rounded-md p-1 w-full h-14',
   })
   return (
     <Link href={href} className="flex items-center h-full" {...props}>
