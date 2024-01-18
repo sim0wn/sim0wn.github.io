d1fa7322d7972ea783fd33679f06a9584af69ffa
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-01-17 23:55:43 -0300

    changed theme switcher layout

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -3,22 +3,30 @@
 import { useTheme } from 'next-themes'
 import SunIcon from '/public/sun.svg'
 import MoonIcon from '/public/moon.svg'
-import { ButtonHTMLAttributes } from 'react'
+import { ButtonHTMLAttributes, useState } from 'react'
 
 export default function ThemeSwitcher({
   ...props
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
   // eslint-disable-next-line perfectionist/sort-objects
   const { theme, setTheme } = useTheme()
+  const [icon, setIcon] = useState(theme === 'dark' ? 'moon' : 'sun')
   return (
     <button
       {...props}
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={() => {
+        const newTheme = theme === 'dark' ? 'light' : 'dark'
+        setTheme(newTheme)
+        setIcon(newTheme === 'dark' ? 'moon' : 'sun')
+      }}
+      onMouseEnter={() => setIcon(theme === 'light' ? 'moon' : 'sun')}
+      onMouseLeave={() => setIcon(theme === 'light' ? 'sun' : 'moon')}
+      className="h-full w-fit rounded-full p-1 text-4xl *:rounded-full hover:bg-neutral-300 dark:hover:bg-white"
     >
-      {theme === 'dark' ? (
-        <SunIcon className="rounded-full bg-yellow-500 p-1 text-orange-700" />
+      {icon === 'moon' ? (
+        <MoonIcon className="bg-sky-800 p-1 text-white" />
       ) : (
-        <MoonIcon className="rounded-full bg-sky-800 p-1 text-white" />
+        <SunIcon className="bg-yellow-500 p-1 text-orange-700" />
       )}
     </button>
   )
