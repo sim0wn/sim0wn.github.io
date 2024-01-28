100095dd666041f566b7cd56aac928f53f83e455
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-01-28 00:31:42 -0300

    added source map building

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '' : undefined,
-  // experimental: {
-  // appDir: true,
   // },
   images: {
     unoptimized: true,
   },
+  // experimental: {
+  // appDir: true,
+  output: 'export',
+  productionBrowserSourceMaps: true,
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
