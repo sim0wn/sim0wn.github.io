02bc49f764aecd0f1c3f7dc2bfb6c3ec5c495df9
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-01-29 16:58:07 -0300

    added a page to tell about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
-import { redirect } from 'next/navigation'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/card'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Sobre mim',
+}
 
 export default function Page() {
-  redirect('/repositories')
+  return (
+    <main className="flex flex-col gap-2">
+      <h1>Sobre mim</h1>
+      <section>
+        <Card>
+          <CardHeader>
+            <CardTitle>Interesses</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ul>
+              <li>Segurança da Informação</li>
+              <li>Redes de Computadores</li>
+              <li>Linux</li>
+            </ul>
+          </CardContent>
+        </Card>
+      </section>
+    </main>
+  )
 }
