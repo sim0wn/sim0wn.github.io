ed4b52f1ee1f91739ee125a5ba8e3c58a2bd2aa9
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-03-05 12:26:33 -0300

    removing cache revalidation from authenticated route

--- src/lib/getRepositories.ts
+++ src/lib/getRepositories.ts
@@ -3,9 +3,6 @@ import { octokit } from '../utils/octokit'
 export async function getRepositories() {
   return (
     await octokit.rest.repos.listForAuthenticatedUser({
-      request: {
-        fetch: (url: string) => fetch(url, { next: { revalidate: 900 } }),
-      },
       sort: 'pushed',
     })
   ).data
